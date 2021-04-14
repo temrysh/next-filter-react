b1bab281e06337684a2256559b5af18f00d682ed
Artem Gavrysh <artem.gavrysh@gmail.com>
2021-04-14 12:34:51 +0200

    vercel test

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,111 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState, memo } from "react"
+import Head from "next/head"
+import Image from "next/image"
+import { useRouter } from "next/router"
+import { FixedSizeList as List } from "react-window"
+import AutoSizer from "react-virtualized-auto-sizer"
+import InfiniteLoader from "react-window-infinite-loader"
+import styles from "../styles/Home.module.css"
 
-export default function Home() {
+export default function Home({ edges, colorFamilies, categoryTags, prices }) {
+  const router = useRouter()
+  const [list, setList] = useState([])
+  const itemCount = edges.length
+
+  const isItemLoaded = ({ index }) => !!list[index]
+
+  const loadMoreItems = ({ startIndex, stopIndex }) =>
+    new Promise((resolve) =>
+      resolve(edges.slice(startIndex, stopIndex))
+    ).then((data) => setList([...list, ...data]))
+
+  const imageLoader = ({ src }) => `http:${src}`
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Next Filter React</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+      <AutoSizer>
+        {({ height, width }) => (
+          <InfiniteLoader
+            isItemLoaded={isItemLoaded}
+            itemCount={itemCount}
+            loadMoreItems={loadMoreItems}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+            {({ onItemsRendered, ref }) => (
+              <List
+                onItemsRendered={onItemsRendered}
+                ref={ref}
+                height={height}
+                width={width}
+                itemSize={500}
+                itemCount={itemCount}
+              >
+                {({ index, style }) => (
+                  <div style={style}>
+                    {list[index] && (
+                      <>
+                        <Image
+                          loader={imageLoader}
+                          width={500}
+                          height={500}
+                          priority
+                          src={list[index].node.thumbnailImage.file.url}
+                          alt={list[index].node.name}
+                        />
+                        <span>{list[index].node.name}</span>
+                      </>
+                    )}
+                  </div>
+                )}
+              </List>
+            )}
+          </InfiniteLoader>
+        )}
+      </AutoSizer>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(
+    "https://dl.dropbox.com/s/iebly5coc7dg8pe/miista-export.json"
+  )
+  const json = await res.json()
+  const { edges } = json.data.allContentfulProductPage
+
+  const colorFamilies = Array.from(
+    edges.reduce((set, { node }) => {
+      node.colorFamily && node.colorFamily.forEach(({ name }) => set.add(name))
+      return set
+    }, new Set())
+  )
+
+  const categoryTags = Array.from(
+    edges.reduce((set, { node }) => {
+      node.categoryTags &&
+        node.categoryTags.forEach((tag) => set.add(tag.trim()))
+      return set
+    }, new Set())
+  )
+
+  const prices = Array.from(
+    edges.reduce((set, { node }) => {
+      node.shopifyProductEu.variants.edges.forEach(({ node: { price } }) =>
+        set.add((price * 100) / 100)
+      )
+      return set
+    }, new Set())
+  )
+
+  return {
+    props: {
+      edges,
+      colorFamilies,
+      categoryTags,
+      prices,
+    },
+  }
 }
