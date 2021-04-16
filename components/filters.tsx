c302ed07711832557353ef80d960eca3ba0113d3
Artem Gavrysh <artem.gavrysh@gmail.com>
2021-04-16 09:04:35 +0200

    filters added

--- components/filters.tsx
+++ components/filters.tsx
@@ -1,45 +1,82 @@
+import { useState } from 'react'
 import { Formik, Form, Field } from "formik"
+import InputRange, { Range } from 'react-input-range'
+import 'react-input-range/lib/css/index.css'
+
+export type FilterMap = {
+  colors: string[]
+  tags: string[]
+  priceRange: Range
+}
 
 type Props = {
   colors: string[]
+  tags: string[]
+  priceLimits: Range
+  onSubmit: (f: FilterMap) => void
 }
 
-const Filters = ({ colors }: Props) => (
-  <div>
-    <Formik
-      initialValues={{ colors: { Yellow: true } }}
-      onSubmit={(values, { setSubmitting }) => {
-        console.log(JSON.stringify(values, null, 2))
-        setSubmitting(false)
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-      }) => (
-        <Form>
-          {colors.map((color) => (
-            <div key={color}>
-              <Field
-                type="checkbox"
-                id={color}
-                name={`colors.${color}`}
-              />
-              <label htmlFor={color}>{color}</label>
+const Filters = ({ colors, tags, priceLimits, onSubmit }: Props) => {
+  const [priceRange, setPriceRange] = useState<Range>(priceLimits)
+  const { min, max } = priceLimits
+
+  return (
+    <div>
+      <Formik
+        initialValues={{ colors: { Yellow: true }, tags: {} }}
+        onSubmit={(values, { setSubmitting }) => {
+          onSubmit({
+            colors: Object.keys(values.colors),
+            tags: Object.keys(values.tags),
+            priceRange
+          })
+          setSubmitting(false)
+        }}
+      >
+        {({ isSubmitting }) => (
+          <>
+            <div>
+              <span>Price range:</span>
+              <InputRange
+                minValue={min}
+                maxValue={max}
+                value={priceRange}
+                onChange={range => setPriceRange(range as Range)} />
             </div>
-          ))}
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-        </button>
-        </Form>
-      )}
-    </Formik>
-  </div>
-)
+            <Form>
+              <div>
+                <span>Colors:</span>
+                {colors.map((color) => (
+                  <div key={color}>
+                    <Field
+                      type="checkbox"
+                      id={color}
+                      name={`colors.${color}`}
+                    />
+                    <label htmlFor={color}>{color}</label>
+                  </div>
+                ))}
+              </div>
+              <div>
+                <span>Tags:</span>
+                {tags.map((tag) => (
+                  <div key={tag}>
+                    <Field
+                      type="checkbox"
+                      id={tag}
+                      name={`tags.${tag}`}
+                    />
+                    <label htmlFor={tag}>{tag}</label>
+                  </div>
+                ))}
+              </div>
+              <button type="submit" disabled={isSubmitting}>Submit</button>
+            </Form>
+          </>
+        )}
+      </Formik>
+    </div>
+  )
+}
 
 export default Filters
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import { Range } from 'react-input-range'
 import styles from "../styles/Home.module.css"
 import Filters from "../components/filters"
 import List from "../components/list"
@@ -8,16 +9,16 @@ type Props = {
   edges: ProductNode[]
   colors: string[]
   tags: string[]
-  prices: string[]
+  priceLimits: Range
 }
 
-const Home = ({ edges, colors, tags, prices }: Props) => (
+const Home = ({ edges, colors, tags, priceLimits }: Props) => (
   <div className={styles.container}>
     <Head>
       <title>Next Filter React</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <Filters colors={colors} />
+    <Filters colors={colors} tags={tags} priceLimits={priceLimits} onSubmit={filters => console.log({ filters })} />
     <List edges={edges} />
   </div>
 )
@@ -31,7 +32,7 @@ export async function getStaticProps() {
   const json: ApiResponse = await res.json()
   const { edges } = json.data.allContentfulProductPage
 
-  const filterTargets = {
+  const filterOptions = {
     colors: new Set(),
     tags: new Set(),
     prices: new Set(),
@@ -39,19 +40,23 @@ export async function getStaticProps() {
 
   edges.reduce((targets, { node }) => {
     node.colorFamily?.forEach(({ name }) => targets.colors.add(name))
-    node.categoryTags?.forEach(tag => targets.tags.add(tag.trim()))
+    node.categoryTags?.forEach(tag => targets.tags.add(tag.trim().toLocaleLowerCase().replace('#', '')))
     node.shopifyProductEu.variants.edges?.forEach(({ node: { price } }) => targets.prices.add(price))
     return targets
-  }, filterTargets)
+  }, filterOptions)
 
-  const [colors, tags, prices] = Object.values(filterTargets).map(s => Array.from(s))
+  const [colors, tags, prices] = Object.values(filterOptions).map(s => Array.from(s).sort())
+  const priceLimits = prices.reduce((acc: Range, price: string) => ({
+    min: Math.min(acc.min, Number(price)),
+    max: Math.max(acc.max, Number(price))
+  }), { min: prices[0], max: prices[0] })
 
   return {
     props: {
       edges,
       colors,
       tags,
-      prices,
+      priceLimits,
     },
   }
 }
